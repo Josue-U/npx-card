90d551ed6aa5ab8d73d10d86b754c7fce95e70dd
Josue-U <josueulliana@gmail.com>
2021-04-21 14:17:04 +0200

    updt

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const chalk = require("chalk");
 const terminalImage = require("terminal-image");
 
 (async () => {
-	console.log(await terminalImage.file('img/Steve_Urkel.jpg', {width: '45%', height:'45%'}));
+	console.log(await terminalImage.file('img/urkel.jpeg', {width: '45%', height:'45%'}));
 })();
 
 const data = {
-  name: chalk.yellow("-----------Josué Ulliana-----------"),
+  name: chalk.yellow("------------------Josué Ulliana------------------"),
   npm: chalk.cyan("https://www.npmjs.com/package/@josue-u"),
   github: chalk.cyan("https://github.com/Josue-U/npx-card"),
   linkTest: chalk.cyan("https://giphy.com/gifs/BxWTWalKTUAdq"),
@@ -21,7 +21,7 @@ const newLine = "\n";
 
 console.log(
   chalk.green(
-    boxen([`${data.name}`, `${data.npm}`, `${data.github}`, `${data.linkTest}`].join(newLine), {
+    boxen([`${data.name}`, `Npm : ${data.npm}`, `Github : ${data.github}`, `? : ${data.linkTest}`].join(newLine), {
       padding: 2,
       margin: 1,
       borderStyle: "round",
